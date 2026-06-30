734501d237a478142186371f19e54353c6a2fe11
victord <victordaniel.123rt@gmail.com>
2026-06-30 08:54:42 -0600

    avances

--- EJERCICIOS/src/main/java/dgtic/core/INBRH26250513/Chicharronera.java
+++ EJERCICIOS/src/main/java/dgtic/core/INBRH26250513/Chicharronera.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Chicharronera {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        double a=  sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
 
    if(a<20000 && a>-20000 && b<20000 && b>-20000 && c<20000 && c>-20000){
        double  sol1=0, sol2=0;
-       int d = (b*b)-4*(a*c);
+       double d = (b*b)-4*(a*c);
        if(d>=0 && a!=0){
            double raiz= Math.sqrt(d);
            sol1= (-b + raiz) /(2*a);
@@ -20,9 +20,7 @@ public class Chicharronera {
            int x1 = (int)Math.round(sol1);
            int x2 = (int)Math.round(sol2);
 
-           int mayor = Math.max(x1,x2);
-           int menor = Math.min(x1,x2);
-           System.out.println((int)mayor + " " + menor);
+           System.out.print(x1 + " " + x2);
         }
       }
     }
