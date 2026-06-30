287e3dededb8f33f432bd42ba1cb9664ccc3c951
victord <victordaniel.123rt@gmail.com>
2026-06-30 09:25:15 -0600

    avances

--- EJERCICIOS/src/main/java/dgtic/core/INBRH26250513/Chicharronera.java
+++ EJERCICIOS/src/main/java/dgtic/core/INBRH26250513/Chicharronera.java
@@ -1,27 +1,28 @@
 package dgtic.core.INBRH26250513;
 
 import java.util.Scanner;
-
+//Esta es la buena  <-----
+//Como nota para los siguientes ejercicios los limites en los parametros no se deben de considerar ni siquiera en los condicionales
+//No van a aparecer.
 public class Chicharronera {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        double a=  sc.nextDouble();
+
+
+        double a = sc.nextDouble();
         double b = sc.nextDouble();
         double c = sc.nextDouble();
 
-   if(a<20000 && a>-20000 && b<20000 && b>-20000 && c<20000 && c>-20000){
-       double  sol1=0, sol2=0;
-       double d = (b*b)-4*(a*c);
-       if(d>=0 && a!=0){
-           double raiz= Math.sqrt(d);
-           sol1= (-b + raiz) /(2*a);
-           sol2= (-b - raiz) /(2*a);
 
-           int x1 = (int)Math.round(sol1);
-           int x2 = (int)Math.round(sol2);
+        double d = (b * b) - 4 * (a * c);
 
-           System.out.print(x1 + " " + x2);
-        }
-      }
+
+        double raiz = Math.sqrt(d);
+
+
+        double sol1 = (-b + raiz) / (2 * a);
+        double sol2 = (-b - raiz) / (2 * a);
+
+        System.out.print(sol1 + " " + sol2);
     }
 }
