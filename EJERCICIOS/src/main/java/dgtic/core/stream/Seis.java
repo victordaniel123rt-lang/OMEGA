b58ca3014a9776a04c3968e3859ce5151caa97b7
victord <victordaniel.123rt@gmail.com>
2026-07-10 17:10:47 -0600

    Stream

--- EJERCICIOS/src/main/java/dgtic/core/stream/Seis.java
+++ EJERCICIOS/src/main/java/dgtic/core/stream/Seis.java
@@ -1,4 +1,14 @@
 package dgtic.core.stream;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Filtrar los strings que empiecen con una vocal y devolverlos en mayúsculas
 public class Seis {
+    public static void main(String[] args){
+        List<String> palabras = Arrays.asList("elefante", "tigre", "iguana", "oso", "mono");
+        List<String> vocales = palabras.stream().filter(p -> p!=null && !p.isEmpty() && p.matches("^[aeiouAEIOUáéíóúÁÉÍÓÚ].*")).collect(Collectors.toList());
+        System.out.print(vocales);
+    }
 }
