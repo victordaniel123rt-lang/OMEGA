57c6aaa6a5133338f9bbf8a6e9a2b0265851f109
victord <victordaniel.123rt@gmail.com>
2026-07-10 17:20:30 -0600

    Stream

--- EJERCICIOS/src/main/java/dgtic/core/stream/Siete.java
+++ EJERCICIOS/src/main/java/dgtic/core/stream/Siete.java
@@ -1,4 +1,21 @@
 package dgtic.core.stream;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+//Separar los números en pares e impares en un Map
 public class Siete {
+    public static void main(String[] args){
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Map<String,List<Integer>> pares = numeros.stream().collect(Collectors.groupingBy(
+                n->(n%2==0)? "Par" : "Impar"
+        ));
+
+        System.out.print(pares);
+
+
+    }
 }
