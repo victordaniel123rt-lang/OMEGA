370cb54500dfc6243a746f2b39550f69e77073a0
victord <victordaniel.123rt@gmail.com>
2026-07-10 16:41:16 -0600

    Stream

--- EJERCICIOS/src/main/java/dgtic/core/stream/Dos.java
+++ EJERCICIOS/src/main/java/dgtic/core/stream/Dos.java
@@ -1,5 +1,19 @@
 package dgtic.core.stream;
 
-public class Dos {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+//En este ejercicio el objetivo es contar cuantas veces se repite una palabra en la lista.
+public class Dos {
+    public static void main(String[] args){
+        List<String> palabras = Arrays.asList("java", "python", "java", "c", "python", "java");
+        Map<String,Long> numeros = palabras.stream().collect(Collectors.groupingBy(
+                Function.identity(),
+                Collectors.counting()
+        ));
+        System.out.print(numeros);
+    }
 }
