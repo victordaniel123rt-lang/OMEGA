1a90e2fb28ce9705f565fc20b3f5a578a7371bd7
victord <victordaniel.123rt@gmail.com>
2026-07-10 16:55:38 -0600

    Stream

--- EJERCICIOS/src/main/java/dgtic/core/stream/Tres.java
+++ EJERCICIOS/src/main/java/dgtic/core/stream/Tres.java
@@ -1,5 +1,15 @@
 package dgtic.core.stream;
 
-public class Tres {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+//En este ejercicio se pretende obtener una lista sin duplicados y después ordenarla en orden alfabetico.
+public class Tres {
+    public static void main(String[] args){
+        List<String> nombres = Arrays.asList("Carlos", "Ana", "Pedro", "Ana", "Carlos");
+        List<String> ordenados = nombres.stream().sorted().distinct().collect(Collectors.toList());
+        System.out.print(ordenados) ;
+    }
 }
